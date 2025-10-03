7ab0b8dc51459d44f7ae41127bb26a54c44393ba
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-10-03 09:25:20 +0200

    Create little free cam script
    this is just a proto to be able to move so taht I can test m things

---
+++ Assets/Scripts/Controller/FreeCamProto.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41e958d606f637e1ce38cf13eeffe0ed9160d7aaed233d51b33562e22f65884d
+size 2028
---
+++ Assets/Scripts/Controller/InputActionsMap.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:27d90c1f16c092026503fef03d4f8a8faa73dfc0af71523183a5e57516e5690b
+size 88558
