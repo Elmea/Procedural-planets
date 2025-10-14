0a14862cf66a71a9277e92643ad8e01f744a8940
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-10-14 23:48:23 +0200

    Optimize chunk creation by creating the normals in the jobs

--- Assets/Scripts/PlanetGen/Chunk.cs
+++ Assets/Scripts/PlanetGen/Chunk.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b00d87a9e32d72442b4ba320f8c88ff518cfee3a502fe32da07f793c2efdafba
-size 4965
+oid sha256:087b6e8148824fe48529a88239b27eff27e7af6ed31dbe9d5e6c34c5fe39d554
+size 6179
