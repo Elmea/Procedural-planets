0f06a44394f00e48f60c34f1213dee47e822f8f3
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-10-14 23:31:00 +0200

    Fix little mem leak

--- Assets/Scripts/PlanetGen/ChunkManager.cs
+++ Assets/Scripts/PlanetGen/ChunkManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47850664efde66481f4ae81990b66ed420157546a7bd63739fddef638cb885a7
-size 6364
+oid sha256:d734c5bd77a6f5719e01562f61eca6d0f8173004bd3f7fa476cb3b878cbb2756
+size 6411
