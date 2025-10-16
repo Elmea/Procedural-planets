400ec1acec3e3a90003b076c512d73cebc21d543
Masseteau Emma <n.masseteau@student.isartdigital.com>
2025-10-16 16:59:18 +0200

    Fix clouds size

--- Assets/Scripts/Clouds/VolumetricCloudManager.cs
+++ Assets/Scripts/Clouds/VolumetricCloudManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55569fb28e88df0623e85a7eee7dc8acc155095558fa6b102b800c8bf09e6b4c
-size 2022
+oid sha256:623ffe5b1782ae9ed78dc4a801e6932a59305295c4e554873cb7f3c58d2874a5
+size 2017
--- Assets/Shaders/VolumetricCloud.shader
+++ Assets/Shaders/VolumetricCloud.shader
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1118e30b66b085fe0675820986792411258a3d8dbafbfae81cff26e56dd969cb
-size 6427
+oid sha256:6750fdf2411f658172c560b787ee2866f9c489364c49fe82b2cad02566a6accb
+size 6411
