c76c036d0926554bd1562d12b2b30f31d338b4de
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-10-26 21:57:18 +0100

    integrate 3D noise + better LOD gestion
    with some bugfix

--- Assets/Scripts/PlanetGen/Chunk.cs
+++ Assets/Scripts/PlanetGen/Chunk.cs
@@ -1,3 +1,5 @@
+using Codice.Client.BaseCommands.Changelist;
+using System.Runtime.CompilerServices;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
@@ -24,7 +26,11 @@ namespace PlanetGen
         private NativeArray<float2> _Uvs;
         private NativeArray<float3> _Normals;
 
-        public void Initialize(int resolution, double chunkSize, Material mat)
+        float _PlanetRadius;
+        double3 _QuadTreeBoundsCenter;
+        double3 _QuadTreeSphereNoRotCenter;
+
+        public void Initialize(int resolution, double chunkSize, Material mat, float planetRadius, double3 qtCenter, double3 qtSphereNoRotCenter)
         {
             _Resolution = math.max(2, resolution);
             _Size = math.max(1f, chunkSize);
@@ -36,6 +42,9 @@ namespace PlanetGen
             _Mesh.MarkDynamic();
             _MeshFilter.sharedMesh = _Mesh;
             _MeshRenderer.sharedMaterial = mat;
+            _PlanetRadius = planetRadius;
+            _QuadTreeBoundsCenter = qtCenter;
+            _QuadTreeSphereNoRotCenter = qtSphereNoRotCenter;
         }
 
         public void Dispose()
@@ -71,15 +80,24 @@ namespace PlanetGen
             _MeshData.SetSubMesh(0, new SubMeshDescriptor(0, indexCount, MeshTopology.Triangles));
 
             // Schedule the vertex job
-            var job = new BuildFlatChunkJob
+            var job = new BuildChunkMeshJob
             {
                 Resolution = _Resolution,
                 Size = _Size,
+                PlanetRadius = _PlanetRadius,
+                QuadTreeSphereNoRotCenter = _QuadTreeSphereNoRotCenter,
+                QuadTreeBoundsCenter = _QuadTreeBoundsCenter,
                 NormalDelta = _Size / (_Resolution * 2.0),
-                WorldPos = new float2(transform.position.x, transform.position.z),
+                WorldPos = (float3)transform.position,
+                WorldRot = (quaternion)transform.rotation,
                 Vertices = _Verts,
                 Normals = _Normals,
-                UVs = _Uvs
+                UVs = _Uvs,
+                Frequency = 0.005f,
+                Amplitude = 100f,
+                Octaves = 5,
+                Lacunarity = 2f,
+                Persistence = 0.5f,
             };
 
             JobHandle handle = job.Schedule(_Verts.Length, 128);
@@ -91,7 +109,8 @@ namespace PlanetGen
 
         public void ApplyMesh()
         {
-            if (!_Verts.IsCreated || !_Uvs.IsCreated) return;
+            if (!_Verts.IsCreated || !_Uvs.IsCreated)
+                return;
 
             var vbPos = _MeshData.GetVertexData<float3>(0);
             var vbNorm = _MeshData.GetVertexData<float3>(1);
@@ -105,16 +124,27 @@ namespace PlanetGen
             _Uvs.Dispose(); // must be disposed or else mem leaks
             _Normals.Dispose(); // must be disposed or else mem leaks
 
+            _Mesh.RecalculateNormals();
             _Mesh.RecalculateBounds();
         }
 
         [BurstCompile]
-        private struct BuildFlatChunkJob : IJobParallelFor
+        private struct BuildChunkMeshJob : IJobParallelFor
         {
             public int Resolution;
+            public float PlanetRadius;
             public double Size;
             public double NormalDelta;
-            public double2 WorldPos;
+            public double3 WorldPos;
+            public quaternion WorldRot;
+            public double3 QuadTreeSphereNoRotCenter;
+            public double3 QuadTreeBoundsCenter;
+
+            public float Frequency;
+            public float Amplitude;
+            public int Octaves;
+            public float Lacunarity;
+            public float Persistence;
 
             public NativeArray<float3> Vertices;
             public NativeArray<float2> UVs;
@@ -123,7 +153,31 @@ namespace PlanetGen
             public void Execute(int index)
             {
                 int vertsPerSide = Resolution + 1;
+                int y = index / vertsPerSide;
+                int x = index % vertsPerSide;
 
+                double fx = (double)x / Resolution;
+                double fy = (double)y / Resolution;
+
+                double xPos = (fx - 0.5) * Size + QuadTreeBoundsCenter.x;
+                double zPos = (fy - 0.5) * Size + QuadTreeBoundsCenter.z;
+
+                double3 planetPoint = new double3(xPos, PlanetRadius, zPos);
+                float3 dir = math.normalize((float3)planetPoint);
+
+                float3 noiseDir = math.mul(WorldRot, dir); // to have a 3d noise that wraps around the sphere instead of local to the face
+                float baseHeight = fbm(noiseDir * Frequency * PlanetRadius, Lacunarity, Octaves, Persistence);
+                float height = baseHeight * Amplitude;
+
+                float3 p = dir * (PlanetRadius + height);
+
+                Vertices[index] = p - (float3)QuadTreeSphereNoRotCenter;
+                UVs[index] = new float2((float)fx, (float)fy);
+            }
+
+            public void ExecuteOld(int index)
+            {
+                int vertsPerSide = Resolution + 1;
                 int y = index / vertsPerSide;
                 int x = index % vertsPerSide;
 
@@ -133,14 +187,35 @@ namespace PlanetGen
                 double xPos = (fx - 0.5) * Size;
                 double zPos = (fy - 0.5) * Size;
 
-                float xHeightL = noise.snoise(new float2((float)(xPos - NormalDelta + WorldPos.x), (float)(zPos + WorldPos.y)) * 0.01f) * 1.0f;
-                float xHeightR = noise.snoise(new float2((float)(xPos + NormalDelta + WorldPos.x), (float)(zPos + WorldPos.y)) * 0.01f) * 1.0f;
-                float zHeightD = noise.snoise(new float2((float)(xPos + WorldPos.x), (float)(zPos - NormalDelta + WorldPos.y)) * 0.01f) * 1.0f;
-                float zHeightU = noise.snoise(new float2((float)(xPos + WorldPos.x), (float)(zPos + NormalDelta + WorldPos.y)) * 0.01f) * 1.0f;
+                float xHeightL = noise.snoise(new float2((float)(xPos - NormalDelta + QuadTreeBoundsCenter.x), (float)(zPos + QuadTreeBoundsCenter.z)) * 0.01f) * 1.0f;
+                float xHeightR = noise.snoise(new float2((float)(xPos + NormalDelta + QuadTreeBoundsCenter.x), (float)(zPos + QuadTreeBoundsCenter.z)) * 0.01f) * 1.0f;
+                float zHeightD = noise.snoise(new float2((float)(xPos + QuadTreeBoundsCenter.x), (float)(zPos - NormalDelta + QuadTreeBoundsCenter.z)) * 0.01f) * 1.0f;
+                float zHeightU = noise.snoise(new float2((float)(xPos + QuadTreeBoundsCenter.x), (float)(zPos + NormalDelta + QuadTreeBoundsCenter.z)) * 0.01f) * 1.0f;
+                //float3 pos = new float3((float)xPos, noise.snoise(new float2((float)(xPos + WorldPos.x), (float)(zPos + WorldPos.y)) * 0.01f) * 1.0f, (float)zPos);
+                double3 pos = new double3(xPos + QuadTreeBoundsCenter.x, PlanetRadius, zPos + QuadTreeBoundsCenter.z);
+                pos = math.normalize(pos);
+                pos *= (PlanetRadius) + noise.snoise(new float2((float)(xPos + QuadTreeBoundsCenter.x), (float)(zPos + QuadTreeBoundsCenter.z)) * 0.01f) * 10.0f;
+                pos -= QuadTreeSphereNoRotCenter; // make relative to world pos
 
-                Vertices[index] = new float3((float)xPos, noise.snoise(new float2((float)(xPos + WorldPos.x), (float)(zPos + WorldPos.y)) * 0.01f) * 1.0f, (float)zPos);
+                Vertices[index] = (float3)pos;
                 Normals[index] = math.normalize(new float3(xHeightL - xHeightR, 2.0f * (float)NormalDelta, zHeightD - zHeightU));
                 UVs[index] = new float2((float)fx, (float)fy);
+            }
+
+            static float fbm(float3 pt, float lacunarity, int octaves, float persistence)
+            {
+                float a = 1f;
+                float amplitude = 0f;
+                float sum = 0f;
+                float3 q = pt * lacunarity;
+                for (int i = 0; i < octaves; i++)
+                {
+                    sum += a * noise.snoise(q);
+                    amplitude += a;
+                    q *= 2f;
+                    a *= persistence;
+                }
+                return sum / math.max(amplitude, 1e-6f);
             }
         }
     }
