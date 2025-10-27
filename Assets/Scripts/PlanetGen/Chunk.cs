977bcba760269e1bb38749381754c18da75c26f9
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-10-27 23:35:57 +0100

    Clean up chunk generation code

--- Assets/Scripts/PlanetGen/Chunk.cs
+++ Assets/Scripts/PlanetGen/Chunk.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5974f434de2bbb6f43b240f1c83f210a51b927ae1e695c6dbd149f8c2c56da7
-size 14747
+oid sha256:168241e2b9d0a081bcf53e92b6682f961b01e13b9b4f14bb75b0b3fde42e423a
+size 14446
