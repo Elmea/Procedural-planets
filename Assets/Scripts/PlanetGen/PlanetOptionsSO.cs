c93cb38d81a884cd9072a4535a893ca0d856e454
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-10-28 23:34:44 +0100

    Create land topography

--- Assets/Scripts/PlanetGen/PlanetOptionsSO.cs
+++ Assets/Scripts/PlanetGen/PlanetOptionsSO.cs
@@ -16,9 +16,9 @@ public class PlanetOptionsSO : ScriptableObject
     [Header("Land/Ocean Basic Settings")]
     [Tooltip("Sea level should be between 0 and 1. 0 being no sea and 1 being full sea")]
     public float SeaLevel = 0.5f;
-    public float SeaCoastWidth = 0.08f;
-    public float LandCoastWidth = 0.08f;
-    
+    public float SeaCoastLimit = 0.46f;
+    public float LandCoastLimit = 0.54f;
+
     [Tooltip("Height of where the land starts. It's used to delimit the coast shelf from the land")]
     public float BaseLandLevel = 10f;
     [Tooltip("Maximum height of the land.")]
@@ -35,31 +35,26 @@ public class PlanetOptionsSO : ScriptableObject
     [Tooltip("Maximum depth of the ocean.")]
     public float OceanMaxDepth = 150f;
 
-    [Header("Inland Settings")]
-    [Tooltip("begin adding hills after this much land-ness")]
-    public float DetailStartMask = 0.35f;
-    [Tooltip("soften the start")]
-    public float DetailRampMask = 0.15f;
-
+    [Header("Hill Settings")]
+    [Tooltip("soften the start of the hills. Will start after the LandCoastWidth.")]
+    public float LandHillRampLimit = 0.6f;
     [Tooltip("This settings is in fraction of the planet radius")]
-    public float HillsWavelength = 0.20f;
+    public float HillsWavelength = 5f;
     public int HillsOctaves = 4;
     public float HillsPersistence = 0.5f;
     public float HillsLacunarity = 2.0f;
-    public float HillsWarpAmplitude = 0.20f;
-    public float HillsWarpFrequency = 1.7f;
     public float HillsAmplitudeMeters = 25f;
 
     // Mountains use ridged noise
-    [Tooltip("Threshold for mountain formation on land noise (0-0.5)")]
-    public float MountainThreshold = 0.35f;
-    public float MountainRamp = 0.05f;
+    [Header("Mountain Settings")]
+    [Tooltip("Start of the mountains")]
+    public float MountainStart = 0.7f;
+    [Tooltip("Threshold for mountain formation on land noise")]
+    public float MountainRampLimit = 0.78f;
     [Tooltip("This settings is in fraction of the planet radius")]
     public float MountainWavelength = 0.06f; // smol
     public int MountainOctaves = 5;
     public float MountainGain = 0.5f; // persistence but for ridged noise
     public float MountainLacunarity = 2.0f;
-    public float MountainWarpAmplitude = 0.15f;
-    public float MountainWarpFrequency = 2.3f;
     public float MountainAmplitudeMeters = 180f;  // in meters
 }
