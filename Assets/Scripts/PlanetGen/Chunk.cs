42e2df1187cf151f809fd772cb0c77872d4fa2fd
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-10-28 23:34:44 +0100

    Create land topography

--- Assets/Scripts/PlanetGen/Chunk.cs
+++ Assets/Scripts/PlanetGen/Chunk.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d20a49ca78327294c547424bfcb4f0e041ad2b55e72f329e006ebbb46451fdd
-size 18203
+oid sha256:0c22b1381cc5a0e795b314ef63fbde385c8e5fdb518580d7cc073d031aa981ee
+size 17255
--- Assets/Scripts/PlanetGen/PlanetOptionsSO.cs
+++ Assets/Scripts/PlanetGen/PlanetOptionsSO.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b54c176b42a8688e7e82542bd5f46191f9fa6dedff9672455865510a721df576
-size 2821
+oid sha256:465594909a98eabcd1d22109a23d6a57dd15aba063652e557d32088a083e3fd6
+size 2642
