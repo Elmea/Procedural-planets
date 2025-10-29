50b0272a8dbd270f4d0d46688aa94145dd53e581
Masseteau Emma <n.masseteau@student.isartdigital.com>
2025-10-29 22:58:52 +0100

    Fix cloud mat inacessible

--- Assets/Scripts/Clouds/VolumetricCloudManager.cs
+++ Assets/Scripts/Clouds/VolumetricCloudManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:623ffe5b1782ae9ed78dc4a801e6932a59305295c4e554873cb7f3c58d2874a5
-size 2017
+oid sha256:22561cdf0ba056df397df8d42dcb6c158464586b70083b353a8d88b289946fa1
+size 2049
