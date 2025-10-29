c0875181693923edb7e769fa67486949c532c6c2
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-10-29 22:43:10 +0100

    Implement a really basic water shader

--- Assets/Scripts/PlanetGen/ChunkManager.cs
+++ Assets/Scripts/PlanetGen/ChunkManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89596aa0da7f533439cb163bae567b101a0f2dd82978e18ab4fe561a0cfacac6
-size 12259
+oid sha256:9714e81623bd5cdaf4f1b946d29be21b5b94572ed5405f701d3e945e6ec6f959
+size 12341
