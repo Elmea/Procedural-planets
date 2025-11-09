42dd664f210741bcb22d57605a7b4eeea47bdbd8
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-11-10 00:42:16 +0100

    Implement the leaves rendering

--- Assets/Scripts/Forest/ForestManager.cs
+++ Assets/Scripts/Forest/ForestManager.cs
@@ -5,23 +5,33 @@ using Unity.Mathematics;
 // https://docs.unity3d.com/6000.2/Documentation/ScriptReference/Graphics.RenderMeshIndirect.html
 public class ForestManager : MonoBehaviour
 {
-    [SerializeField] Material _Material;
+    [SerializeField] Material _BarkMaterial;
+    [SerializeField] Material _LeavesMaterial;
     [SerializeField] Mesh _Mesh;
     [SerializeField] uint _NumberOfInstances = 10;
 
-    GraphicsBuffer _CommandBuffer;
+    GraphicsBuffer _BarkCommandBuffer;
+    GraphicsBuffer _LeavesCommandBuffer;
     GraphicsBuffer _TransformBuffer;
 
-    GraphicsBuffer.IndirectDrawIndexedArgs[] commandData;
+    GraphicsBuffer.IndirectDrawIndexedArgs[] _BarkCommandData;
+    GraphicsBuffer.IndirectDrawIndexedArgs[] _LeavesCommandData;
 
     void Start()
     {
-        _CommandBuffer = new GraphicsBuffer(
+        _BarkCommandBuffer = new GraphicsBuffer(
             GraphicsBuffer.Target.IndirectArguments,
             1,
             GraphicsBuffer.IndirectDrawIndexedArgs.size
         );
-        commandData = new GraphicsBuffer.IndirectDrawIndexedArgs[1];
+        _LeavesCommandBuffer = new GraphicsBuffer(
+            GraphicsBuffer.Target.IndirectArguments,
+            1,
+            GraphicsBuffer.IndirectDrawIndexedArgs.size
+        );
+
+        _BarkCommandData = new GraphicsBuffer.IndirectDrawIndexedArgs[1];
+        _LeavesCommandData = new GraphicsBuffer.IndirectDrawIndexedArgs[1];
 
         _TransformBuffer = new GraphicsBuffer(
             GraphicsBuffer.Target.Structured,
@@ -32,28 +42,38 @@ public class ForestManager : MonoBehaviour
         float4x4[] instanceData = new float4x4[_NumberOfInstances];
         for (int i = 0; i < _NumberOfInstances; i++)
         {
-            float x = i * 1.5f;
+            float x = i * 10f;
             float y = 0f;
             float z = 0f;
-            float scale = 1.0f;
+            float scale = 0.25f;
             instanceData[i] = float4x4.TRS(new float3(x, y, z), quaternion.identity, new float3(scale, scale, scale));
         }
 
         _TransformBuffer.SetData(instanceData);
 
-        commandData[0].indexCountPerInstance = _Mesh.GetIndexCount(0);
-        commandData[0].instanceCount = _NumberOfInstances;
-        commandData[0].startIndex = (uint)_Mesh.GetIndexStart(0);
-        commandData[0].baseVertexIndex = (uint)_Mesh.GetBaseVertex(0);
-        commandData[0].startInstance = 0;
+        _BarkCommandData[0].indexCountPerInstance = _Mesh.GetIndexCount(0);
+        _BarkCommandData[0].instanceCount = _NumberOfInstances;
+        _BarkCommandData[0].startIndex = (uint)_Mesh.GetIndexStart(0);
+        _BarkCommandData[0].baseVertexIndex = (uint)_Mesh.GetBaseVertex(0);
+        _BarkCommandData[0].startInstance = 0;
 
-        _CommandBuffer.SetData(commandData);
+        _LeavesCommandData[0].indexCountPerInstance = _Mesh.GetIndexCount(1);
+        _LeavesCommandData[0].instanceCount = _NumberOfInstances;
+        _LeavesCommandData[0].startIndex = (uint)_Mesh.GetIndexStart(1);
+        _LeavesCommandData[0].baseVertexIndex = (uint)_Mesh.GetBaseVertex(1);
+        _LeavesCommandData[0].startInstance = 0;
+
+        _BarkCommandBuffer.SetData(_BarkCommandData);
+        _LeavesCommandBuffer.SetData(_LeavesCommandData);
     }
 
     void OnDestroy()
     {
-        _CommandBuffer?.Release();
-        _CommandBuffer = null;
+        _BarkCommandBuffer?.Release();
+        _BarkCommandBuffer = null;
+
+        _LeavesCommandBuffer?.Release();
+        _LeavesCommandBuffer = null;
 
         _TransformBuffer?.Release();
         _TransformBuffer = null;
@@ -61,15 +81,18 @@ public class ForestManager : MonoBehaviour
 
     void Update()
     {
-        if (_Mesh == null || _Material == null)
+        if (_Mesh == null || _BarkMaterial == null)
             return;
 
-        var rp = new RenderParams(_Material);
+        var rp = new RenderParams(_BarkMaterial);
         rp.worldBounds = new Bounds(Vector3.zero, 10000f * Vector3.one);
         rp.matProps = new MaterialPropertyBlock();
         rp.matProps.SetBuffer("_TransformBuffer", _TransformBuffer);
         rp.matProps.SetMatrix("_ObjectToWorld", transform.localToWorldMatrix);
 
-        Graphics.RenderMeshIndirect(rp, _Mesh, _CommandBuffer, 1);
+        Graphics.RenderMeshIndirect(rp, _Mesh, _BarkCommandBuffer, 1);
+
+        rp.material = _LeavesMaterial;
+        Graphics.RenderMeshIndirect(rp, _Mesh, _LeavesCommandBuffer, 1);
     }
 }
--- Assets/Shaders/TreeInstanced.shader
+++ Assets/Shaders/TreeInstanced.shader
@@ -3,8 +3,8 @@ Shader "Custom/TreeInstanced"
     Properties
     {
         _MainTex    ("Albedo (RGB)", 2D) = "white" {}
-        _Glossiness ("Smoothness", Range(0,1)) = 0.5
-        _Metallic   ("Metallic", Range(0,1)) = 0.0
+        _MaskTex    ("Roughness-Metallic Map", 2D) = "white" {}
+        _BumpMap    ("Normal Map", 2D) = "bump" {}
     }
     SubShader
     {
@@ -21,6 +21,8 @@ Shader "Custom/TreeInstanced"
             #include "UnityIndirect.cginc"
 
             sampler2D _MainTex;
+            sampler2D _MaskTex;
+            sampler2D _BumpMap;
             float4 _MainTex_ST;
             float _Glossiness;
             float _Metallic;
@@ -32,15 +34,18 @@ Shader "Custom/TreeInstanced"
             {
                 float4 vertex : POSITION;
                 float3 normal : NORMAL;
+                float4 tangent : TANGENT;
                 float2 uv     : TEXCOORD0;
             };
 
             struct v2f
             {
-                float4 pos          : SV_POSITION;
-                float2 uv           : TEXCOORD0;
-                float3 worldPos     : TEXCOORD1;
-                float3 worldNormal  : TEXCOORD2;
+                float4 pos           : SV_POSITION;
+                float2 uv            : TEXCOORD0;
+                float3 worldPos      : TEXCOORD1;
+                float3 worldNormal   : TEXCOORD2;
+                float3 worldTangent  : TEXCOORD3;
+                float3 worldBitangent: TEXCOORD4;
             };
 
             v2f vert(appdata v, uint svInstanceID : SV_InstanceID)
@@ -51,21 +56,42 @@ Shader "Custom/TreeInstanced"
                 float4x4 inst = _TransformBuffer[instanceID];
                 float4x4 model = mul(_ObjectToWorld, inst);
                 float4 worldPos = mul(model, v.vertex);
-                float3 worldNormal = normalize(mul((float3x3)model, v.normal));
+
+                float3 worldNormal  = normalize(mul((float3x3)model, v.normal));
+                float3 worldTangent = normalize(mul((float3x3)model, v.tangent.xyz));
+                float3 worldBitangent = normalize(cross(worldNormal, worldTangent) * v.tangent.w);
 
                 v2f o;
                 o.pos = mul(UNITY_MATRIX_VP, worldPos);
                 o.uv  = TRANSFORM_TEX(v.uv, _MainTex);
                 o.worldPos = worldPos.xyz;
+
                 o.worldNormal = worldNormal;
+                o.worldTangent = worldTangent;
+                o.worldBitangent = worldBitangent;
                 return o;
             }
 
             float4 frag(v2f i) : SV_Target
             {
                 float3 albedo = tex2D(_MainTex, i.uv).rgb;
+                float alpha = tex2D(_MainTex, i.uv).a;
+                if (alpha < 0.1)
+                    discard;
+
+                float4 maskSample = tex2D(_MaskTex, i.uv);
+                float smoothness  = maskSample.g;
+                float metallic    = maskSample.b;
+
+                float3 tNormal = UnpackNormal(tex2D(_BumpMap, i.uv));
 
                 float3 N = normalize(i.worldNormal);
+                float3 T = normalize(i.worldTangent);
+                float3 B = normalize(i.worldBitangent);
+
+                float3x3 TBN = float3x3(T, B, N);
+                N = normalize(mul(tNormal, TBN));
+
                 float3 L = _WorldSpaceLightPos0.xyz;
                 float3 V = normalize(_WorldSpaceCameraPos - i.worldPos);
 
@@ -75,8 +101,8 @@ Shader "Custom/TreeInstanced"
                 float3 ambient = ShadeSH9(float4(N, 1.0));
 
                 float3 H = normalize(L + V);
-                float spec = pow(saturate(dot(N, H)), lerp(16, 128, _Glossiness));
-                float3 specCol = _LightColor0.rgb * spec * lerp(0.04, 1.0, _Metallic);
+                float spec = pow(saturate(dot(N, H)), lerp(0, 128, smoothness));
+                float3 specCol = _LightColor0.rgb * spec * lerp(0.04, 1.0, metallic);
 
                 float3 color = diffuse + ambient * albedo + specCol;
                 return float4(color, 1);
