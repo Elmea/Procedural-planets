58570252fce31ceb9ff2b6da41b5552b69bf63b0
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-11-09 23:30:03 +0100

    Testing the Graphics.RenderMeshIndirect API

---
+++ Assets/Scripts/Forest/ForestManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c22ccad362a1d808df23b3874ba021d86eb9d1e60a224cd5edc68e6eb88b82a
+size 2411
---
+++ Assets/Shaders/TreeInstanced.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0a5dadad988a220095053e6aee60aa90cf192fe7bc9b704ec1bd0621004abdf
+size 2779
