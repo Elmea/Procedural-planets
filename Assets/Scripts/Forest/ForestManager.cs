f21cef9eaf4fdc4fd883bc602eadc30417611663
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-11-11 23:13:02 +0100

    Add GPU culling for trees

--- Assets/Scripts/Forest/ForestManager.cs
+++ Assets/Scripts/Forest/ForestManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:994a64150c492bdeb1f71d6d29358fe8da507433d38b173029bdcff5c8a183ae
-size 4527
+oid sha256:8b3549b5ac8f61002d91f0529974ff2c20711507b1c72e3d8457d7df6f2fc11d
+size 5447
